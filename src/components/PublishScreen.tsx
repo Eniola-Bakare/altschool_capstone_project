870ae14220a342f170971ee35df3ed7108c61ad6
eniola <eebakare@gmail.com>
2024-03-13 15:19:34 +0100

    publish screen - attached text and image

--- src/components/PublishScreen.tsx
+++ src/components/PublishScreen.tsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function PublishScreen() {
   return (
-    <section className="w-[90%] h-[85%] border border-borderGrey rounded-lg flex flex-col py-14 px-16 gap-16">
+    <section className="w-[90%] h-[85%] border border-borderGrey rounded-lg flex flex-col py-14 px-16 gap-12">
       <div className="buttn-div self-end">
         <Button
           type="primary"
@@ -11,7 +11,9 @@ function PublishScreen() {
         />
       </div>
 
-      <div className="input-div flex items-center gap-2">
+      <div className="input-div h-full flex items-center gap-2">
+        {/* publish screen 1 */}
+
         {/* <img src="./src/assets/publishIcon.png" alt="plus icon" />
 
         <div className="input-Text flex flex-col gap-2">
@@ -27,7 +29,9 @@ function PublishScreen() {
           />
         </div> */}
 
-        <div className="h-[120px] close-div pr-3 border-r border-b-grey flex justify-center items-center ">
+        {/* publish screen 2 */}
+
+        {/* <div className="h-[120px] close-div pr-3 border-r border-b-grey flex justify-center items-center ">
           <div className="">
             <img
               src="./src/assets/closeIcon.png"
@@ -40,7 +44,19 @@ function PublishScreen() {
         <div className="attachment-icons flex pl-3 gap-3">
           <img src="./src/assets/imgIcon.png" alt="an image icon" />
           <img src="./src/assets/vidIcon.png" alt="a video icon" />
-        </div>
+        </div> */}
+
+        {/* publish screen three */}
+          <div className="publish-screen-three w-full h-full flex flex-col items-start gap-4">
+            <img
+              src="./src/assets/publishImage.png"
+              alt="an attached image"
+              className=""
+            />
+          <textarea placeholder="Write a post......" className="w-full h-full focus:outline-0 " />
+          </div>
+
+        {/* publish screen 4 */}
       </div>
     </section>
   );
