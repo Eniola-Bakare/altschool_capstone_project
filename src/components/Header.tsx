52c7f7c1b4eeb2f09182b72d2e5b24dbb5945128
eniola <eebakare@gmail.com>
2024-03-21 15:32:39 +0100

    phone screen: responsive,. Up next - nav btn func

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,11 @@ function Header() {
     section.scrollIntoView({ behavior: "smooth" });
   }
   return (
-    <nav className="w-full bg-white flex justify-between items-center py-4 lg:px-10 xl:px-20">
+    <nav className="w-full bg-white flex justify-between items-center py-4 px-4 md:px-8 lg:px-10 xl:px-20">
       <Link to="/">
         <LogoText />
       </Link>
-      <ul className="navLinks flex gap-4">
+      <ul className="navLinks gap-4 hidden lg:flex">
         <li className="text-black font-bold ">Home</li>
         <li className="text-black font-bold " onClick={() => handleScroll()}>
           About us
@@ -20,8 +20,9 @@ function Header() {
         <li className="text-black font-bold ">Contact</li>
         <li className="text-black font-bold ">Blogs</li>
       </ul>
+      <img src="/hamburger.png" alt="an hamburger icon" className="w-[45px]" />
 
-      <span className="buttons flex gap-2">
+      <span className="buttons gap-2 hidden lg:flex">
         <Link to="/signin">
           <Button name="Log in" type="secondary" />
         </Link>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -9,19 +9,21 @@ function LandingPage() {
     <div className="w-full flex flex-col items-center ">
       <Header />
       <section
-        className="w-full hero-section bg-cover bg-left-top h-screen flex flex-col justify-center"
+        className="w-full hero-section bg-cover bg-left-top h-[90vh] flex flex-col justify-center px-8 lg:px-0"
         style={{
           backgroundImage:
             "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),url('heroImage.png')",
         }}
       >
-        <div className="hero-text self-center  flex flex-col gap-10">
-          <h1 className="text-5xl font-bold text-white leading-[72px]">
+        <div className="hero-text w-ful md:w-[85%] lg:w-full self-center  flex flex-col gap-10">
+          <h1 className=" text-[32px] md:text-5xl lg:text-5xl font-bold text-white leading-[40px] md:leading-[72px]">
             Welcome to Chatter: A Haven for Text-
-            <br />
+            <span className="md:hidden lg:visible">
+              <br />
+            </span>
             Based Content
           </h1>
-          <p className="text-white text-2xl font-medium w-[75%] leading-[36px]">
+          <p className="text-white text-lg md:text-2xl font-medium md:w-[75%] md:leading-[36px]">
             Unleash the Power of Words, Connect with Like-minded Readers and
             Writers
           </p>
@@ -36,12 +38,14 @@ function LandingPage() {
       </section>
 
       <section
-        className="about-sec flex lg:px-10 lg:py-8 xl:py-16 xl:px-20 justify-between"
+        className="about-sec flex flex-col lg:flex-row md:px-12 py-12 lg:px-10 lg:py-8 xl:py-16 xl:px-20 justify-between items-center  gap-6 md:gap-16 lg:gap-0"
         id="about"
       >
-        <div className="about-text lg:w-[58%] 2xl:w-[50%] flex flex-col gap-8 text-black">
-          <h1 className="text-5xl font-bold ">About Chatter</h1>
-          <p className="text-lg">
+        <div className="about-text w-[90%] md:w-full lg:w-[58%] 2xl:w-[50%] flex flex-col gap-4 md:gap-8 text-black">
+          <h1 className="text-5xl font-bold text-center md:text-left ">
+            About Chatter
+          </h1>
+          <p className=" text-lg text-justify md:text-left ">
             Chatter is a multi-functional platform where authors and readers can
             have access to their own content. It aims to be a traditional
             bookworm’s heaven and a blog to get access to more text based
@@ -51,7 +55,7 @@ function LandingPage() {
             beliefs. By promoting dialogue and understanding, we strive{" "}
           </p>
         </div>
-        <div className="about-image lg:w-[35%] 2xl:w-[35%]">
+        <div className="about-image w-[90%] lg:w-[35%] 2xl:w-[35%]">
           <img
             src="aboutImage.png"
             alt="a group of people on a stair way"
@@ -60,9 +64,11 @@ function LandingPage() {
         </div>
       </section>
 
-      <section className="why-sec lg:px-10 lg:py-8 xl:py-16 xl:px-20 flex flex-col items-center gap-8 lg:w-[80%] 2xl:w-[75%]">
-        <h1 className="text-5xl font-bold ">Why you should join chatter</h1>
-        <p className="xl:w-[85%] 2xl:w-[70%]">
+      <section className="why-sec md:px-12 py-12 lg:px-10 lg:py-8 xl:py-16 xl:px-20 flex flex-col items-center gap-8 lg:w-[80%] 2xl:w-[75%]">
+        <h1 className="w-[80%] md:w-full text-4xl md:text-5xl font-bold text-center md:text-left ">
+          Why you should join chatter
+        </h1>
+        <p className="w-[95%] xl:w-[85%] 2xl:w-[70%] text-justify md:text-left ">
           Our goal is to make writers and readers see our platform as their next
           heaven for blogging, ensuring ease in interactions, connecting with
           like-minded peers, have access to favorite content based on interests
@@ -70,75 +76,73 @@ function LandingPage() {
         </p>
       </section>
 
-      <section className="card-sec sm:pb-20 flex lg:gap-7 xl:gap-12">
-        <div className="each-card w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center  items-start gap-3">
+      <section className="card-sec sm:pb-20 flex flex-col items-center lg:items-start gap-7 lg:flex-row lg:gap-7 xl:gap-12 py-12 lg:py-0">
+        <div className="each-card w-[90%] md:w-[650px] lg:w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center items-center md:items-start gap-3">
           <img src="iconOne.png" alt="anlytic icon" />
           <header className="text-2xl font-medium leading-[36px]">
             Analytics
           </header>
-          <p className="text-lg text-grey">
+          <p className="text-lg text-center md:text-start text-grey w-[85%] lg:w-full">
             Analytics to track the number of views, likes and comment and also
             analyze the performance of your articles over a period of time
           </p>
         </div>
-        <div className="each-card w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center  items-start gap-3">
+        <div className="each-card w-[90%] md:w-[650px] lg:w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center items-center md:items-start gap-3">
           <img src="iconTwo.png" alt="anlytic icon" />
           <header className="text-2xl font-medium leading-[36px]">
             Social interactions
           </header>
-          <p className="text-lg text-grey">
+          <p className="text-lg text-center md:text-start text-grey w-[85%] lg:w-full">
             Users on the platform can interact with posts they like, comment and
             engage in discussions
           </p>
         </div>
-        <div className="each-card w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center  items-start gap-3">
+        <div className="each-card w-[90%] md:w-[650px] lg:w-[306px] h-[324px] py-4 px-5 rounded-lg border border-boderGrey flex flex-col justify-center items-center md:items-start gap-3">
           <img src="iconThree.png" alt="anlytic icon" />
           <header className="text-2xl font-medium leading-[36px]">
             Content creation
           </header>
-          <p className="text-lg text-grey">
+          <p className="text-lg text-center md:text-start text-grey w-[85%] lg:w-full">
             Write nice and appealing with our in-built markdown, a rich text
             editor
           </p>
         </div>
       </section>
 
-      <section className="testimony bg-rgba flex items-center justify-center w-full h-[492px]">
-        <div className="review-image">
-          <img
-            src="reviewImage.png"
-            alt="a man"
-            className="rounded-full xl:w-[70%]"
-          />
+      <section className="testimony bg-rgba flex flex-col md:flex-row items-center justify-center px-4 md:px-0 py-12 md:py-0 w-full md:h-[492px] gap-8 md:gap-0">
+        <div className="review-image w-[45%] md:w-[25%] xl:w-[40%]">
+          <img src="reviewImage.png" alt="a man" className="rounded-full " />
         </div>
-        <div className="testimonial-text justify-center w-[60%] lg:py-8 lg:px-10 xl:py-16 xl:px-20 flex flex-col gap-9 ">
-          <p className="text-lg leading-7 text-black">
+        <div className="testimonial-text justify-center md:w-[65%] lg:w-[60%] md:py-4 md:px-8 lg:py-8 lg:px-10 xl:py-16 xl:px-20 flex flex-col gap-9 ">
+          <p className="text-lg leading-7 text-center md:text-left text-black">
             "Chatter has become an integral part of my online experience. As a
             user of this incredible blogging platform, I have discovered a
             vibrant community of individuals who are passionate about sharing
             their ideas and engaging in thoughtful discussions.”
           </p>
 
-          <p className="font-medium text-4xl">
+          <p className="font-medium text-center md:text-center text-2xl md:text-4xl">
             Adebobola Muhydeen,
             <br />
-            <span className="font font-normal text-2xl">
+            <span className="font font-normal text-lg md:text-center md:text-2xl">
               Software developer at Apple
             </span>
           </p>
-          <Link to="/signup">
-            <Button
-              type="primary"
-              name="Join chatter"
-              onClick={() => console.log("join chatter")}
-            />
-          </Link>
+          <span className="flex justify-center md:justify-start">
+            <Link to="/signup">
+              <Button
+                type="primary"
+                name="Join chatter"
+                onClick={() => console.log("join chatter")}
+              />
+            </Link>
+          </span>
         </div>
       </section>
 
-      <section className="profiles w-full bg-white flex items-center justify-center h-[492px]">
-        <div className="profile-text flex lg:w-[90%] xl:w-[70%] gap-20">
-          <div className="all-three-profiles flex lg:w-[55%] gap-8 items-center">
+      <section className="profiles w-full bg-white flex items-center justify-center md:h-[492px]">
+        <div className="profile-text flex flex-col items-center md:flex-row w-full md:w-[85%] px-4 md:px-0 py-12 md:py-0 xl:w-[70%] gap-10 lg:gap-20">
+          <div className="all-three-profiles flex w-[70%] lg:w-[55%] lg:gap-8 items-center">
             <div className="two-profiles flex flex-col gap-14 justify-between items-center ">
               <img
                 src="profileOne.png"
@@ -156,8 +160,8 @@ function LandingPage() {
             </div>
           </div>
 
-          <div className="profiles-text flex flex-col gap-6">
-            <h1 className="text-black lg:text-4xl xl:text-5xl font-bold leading-[72px]">
+          <div className="profiles-text flex flex-col text-center md:text-left gap-6 justify-center">
+            <h1 className="text-black text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold leading-[30px] md:leading-[72px]">
               Write, read and connect <br /> with great minds on chatter{" "}
             </h1>
             <p className="lg:text-xl xl:text-lg">
