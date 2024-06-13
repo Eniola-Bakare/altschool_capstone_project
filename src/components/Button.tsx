13512822804b974fb9289b12015b6fad1d4bbf1a
eniola <eebakare@gmail.com>
2024-06-13 14:05:05 +0100

    Comment feat: input field

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   type: "primary" | "secondary";
   width?: string;
   disabled: boolean;
+  height?: string;
 };
 function Button({
   name,
@@ -35,7 +36,16 @@ function Button({
   return (
     <button
       onClick={onClick}
-      className="w-[157px] py-2 px-4 bg-white border-2 border-blue text-black rounded-lg font-bold"
+      // className="w-[157px] py-2 px-4 bg-white border-2 border-blue text-black rounded-lg font-bold"
+      className={
+        width
+          ? `${width} ${
+              disabled ? "cursor-not-allowed" : " cursor-pointer"
+            } h-[56px] py-2 px-4 bg-white border-2 border-blue text-black rounded-lg font-bold`
+          : `${
+              disabled ? "cursor-not-allowed" : "cursor-pointer"
+            } w-[157px] py-2 px-4 bg-white border-2 border-blue text-black rounded-lg font-bold`
+      }
     >
       {name}
     </button>
--- src/components/SubPages/CommentSection/CommentInput.tsx
+++ src/components/SubPages/CommentSection/CommentInput.tsx
@@ -4,24 +4,26 @@ import { useAuthContext } from "../../contexts/AuthContext";
 function CommentInput() {
   const { currentUser } = useAuthContext();
   return (
-    <div className="flex items-center justify-start w-full">
+    <div className="flex items-center justify-between w-full gap-1 relative">
       <img
         src={currentUser?.photoURL}
         alt="current user"
-        className="rounded-full w-7"
+        className="rounded-full w-8"
       />
       <input
         type="text"
         placeholder="type a comment"
-        className="border-2 rounded-xl p-1 pl-2 h-[56px]"
+        className="border-2 rounded-xl p-1 pl-2 h-[56px] w-[95%]"
       />
-      <Button
-        type="secondary"
-        name=">"
-        width="w"
-        onClick={() => console.log("posting")}
-        disabled
-      />
+      <div className=" absolute right-0">
+        <Button
+          type="primary"
+          name=">"
+          width="w"
+          onClick={() => console.log("posting")}
+          disabled={false}
+        />
+      </div>
     </div>
   );
 }
